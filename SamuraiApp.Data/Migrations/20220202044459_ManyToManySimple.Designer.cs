8d4816bc47988128be6a6cd97fbbd7d88d565b23
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-02-02 10:17:57 +0530

    Add many to many relationship

--- SamuraiApp.Data/Migrations/20220202044459_ManyToManySimple.Designer.cs
+++ SamuraiApp.Data/Migrations/20220202044459_ManyToManySimple.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SamuraiApp.Data;
 
 namespace SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20220202044459_ManyToManySimple")]
+    partial class ManyToManySimple
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
