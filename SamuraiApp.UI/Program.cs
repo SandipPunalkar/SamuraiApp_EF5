cb41972a4bc1d7203e400790b30ed14507e50093
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-03-05 18:46:41 +0530

    Adding Views and Other Database Objects Using Migrations

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -50,7 +50,7 @@ namespace SamuraiApp.UI
             //ReturnAllBattleWithSamurais();
             //AddAllSamuraisToAllBattles();
 
-            RemoveSamuraiFromABattle();
+           // RemoveSamuraiFromABattle();
 
         }
 
